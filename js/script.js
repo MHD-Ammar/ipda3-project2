f58e49673acccfdc52521019eef92d3e69f792c7
MHD-Ammar <ammarsati1998@gmail.com>
2020-08-30 16:02:20 +0200

    home page articles login-ar loging-ar nwe-accont-ar added

--- js/script.js
+++ js/script.js
@@ -20,5 +20,30 @@ $(document).ready(function () {
                 loop: true
             }
         }
-    })
+    })   
+
+
+    $('.owl-carousel.owl-carousel-articles-page').owlCarousel({
+        loop: true,
+        margin: 10,
+        rtl:true,
+        responsiveClass: true,
+        autoplay:true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            600: {
+                items: 2,
+                nav: false
+            },
+            1000: {
+                items: 3,
+                nav: true,
+                loop: true
+            }
+        }
+    })  
+     
 });
